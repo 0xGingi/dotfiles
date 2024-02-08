dad762895436373045307f44f4f6cc555d0f1dff
0xgingi <0xgingi@0xgingi.com>
2024-02-07 20:29:06 -0500

    swap to nwg-panel

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ fi
 
 yay -S --needed --noconfirm qt5-wayland qt5ct qt6-wayland qt6ct qt5-svg qt5-quickcontrols2 qt5-graphicaleffects gtk3 polkit-kde-agent pipewire jq wl-clipboard cliphist python-requests pacman-contrib
 yay -S --needed --noconfirm spdlog-git sdbus-cpp
-yay -S --needed --noconfirm hyprland-git kitty mako nwg-panel nvim wofi xdg-desktop-portal-hyprland-git xdg-desktop-portal-gtk hyprpaper neovim swappy grim slurp btop pamixer pavucontrol network-manager-applet starship papirus-icon-theme indicator-kdeconnect-git kdeconnect ttf-jetbrains-mono-nerd noto-fonts-emoji lxappearance nwg-look-bin cliphist starship nautilus swaylock-effects wlogout brightnessctl bluez bluez-utils blueman gvfs
+yay -S --needed --noconfirm hyprland-git kitty mako nwg-panel nvim wofi xdg-desktop-portal-hyprland-git xdg-desktop-portal-gtk hyprpaper neovim swappy grim slurp btop pamixer pavucontrol network-manager-applet starship papirus-icon-theme indicator-kdeconnect-git kdeconnect ttf-jetbrains-mono-nerd noto-fonts-emoji lxappearance nwg-look-bin cliphist starship dolphin swaylock-effects wlogout brightnessctl bluez bluez-utils blueman gvfs
 clear
 
 echo 'backing up existing configs'
